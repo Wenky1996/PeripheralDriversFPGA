9ea9c67cbb20d76865418f0639e4b735ae89453b
wenky <wenkyjong1996@gmail.com>
2024-09-18 22:31:44 +0800

    spi finish

--- SD_Drivers/src/verilog/sd_top.v
+++ SD_Drivers/src/verilog/sd_top.v
@@ -14,7 +14,26 @@ module sd_top (
     input   sclk,
     input   rst_n,
     output  sd_clk,
-    output 
+    output  spi_mosi
 );
-    
+
+spi # (
+    .CPOL(1),
+    .CPAH(1)
+  )
+  spi_inst (
+    .clk                               (clk                                    ),
+    .spi_clk_div                       (spi_clk_div                            ),
+    .rst_n                             (rst_n                                  ),
+    .data_write                        (data_write                             ),
+    .write_en                          (write_en                               ),
+    .read_en                           (read_en                                ),
+    .spi_miso                          (spi_miso                               ),
+    .data_read                         (data_read                              ),
+    .spi_clk                           (spi_clk                                ),
+    .spi_cs                            (spi_cs                                 ),
+    .write_busy                        (write_busy                             ),
+    .read_busy                         (read_busy                              ),
+    .spi_mosi                          (spi_mosi                               )
+  );
 endmodule

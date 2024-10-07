6173a5dae5f5af8874a2e7c11803469db591213b
wenky <wenkyjong@gmail.com>
2024-10-07 22:09:34 +0800

    spi simulation finish

---
+++ SD_Drivers/rtl.f
@@ -0,0 +1,3 @@
+/home/ICer/ic_prjs/PeripheralDriversFPGA/SD_Drivers/tb/tb_spi.sv
+/home/ICer/ic_prjs/PeripheralDriversFPGA/SD_Drivers/src/verilog/sd_top.v
+/home/ICer/ic_prjs/PeripheralDriversFPGA/SD_Drivers/src/verilog/spi.v
